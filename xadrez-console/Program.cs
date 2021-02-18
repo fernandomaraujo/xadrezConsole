b07cdd7239e50e627a6faf8e460c5e669a7b4596
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2021-02-18 17:49:47 -0300

    Implementação inicial do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -7,8 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            
 
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Console.ReadLine();
 
         }
     }
