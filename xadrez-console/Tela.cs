4e3251c780e069d86d6c6553100a3aff07edc934
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2021-02-18 18:32:19 -0300

    Implementação inicial da tela do tabuleiro

---
+++ xadrez-console/Tela.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Tela
+    {
+
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            // Tabuleiro
+            int i, j;
+            
+            // Linhas
+            for(i = 0; i<tab.linhas; i++)
+            {
+                // Colunas
+                for (j = 0; j < tab.colunas; j++)
+                {
+
+                    // Se não houver peça na posição linha e coluna
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write(" - ");
+                    }
+                    else {
+
+                        Console.Write(tab.peca(i, j) + " ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
