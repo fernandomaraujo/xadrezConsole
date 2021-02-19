3d20d296225f08d46371fa3be62d86060264ca05
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2021-02-19 16:47:54 -0300

    Criada classe PosicaoXadrez e lógica de posições

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,6 +8,8 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+
+            /*
             try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
@@ -22,7 +24,12 @@ namespace xadrez_console
             catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
-            }
+            } */
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.paraPosicao());
 
             Console.ReadLine();
         }
---
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,32 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        // Convertendo posição de xadrez para uma posição matriz
+        public Posicao paraPosicao()
+        {
+            // Para a coluna, operações com os códigos das letras (utilizando tabela ASCII)
+            // docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/strings/#related-topics
+
+            // Só funciona com letras minúsculas
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
