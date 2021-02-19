093c9bea85f39f29e0ad84979f15923d12aaf297
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2021-02-19 17:23:14 -0300

    Melhorando impressão do tabuleiro

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -14,6 +14,9 @@ namespace xadrez_console
             // Linhas
             for(i = 0; i<tab.linhas; i++)
             {
+                // Exibindo número da coluna
+                Console.Write(" " + (8 - i) + " |");
+
                 // Colunas
                 for (j = 0; j < tab.colunas; j++)
                 {
@@ -25,12 +28,44 @@ namespace xadrez_console
                     }
                     else {
 
-                        Console.Write(tab.peca(i, j) + " ");
+                        // Console.Write(tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
 
                 Console.WriteLine();
             }
+
+            Console.WriteLine("----------------------------");
+            // Console.WriteLine("___________________________");
+            Console.WriteLine("   | a  b  c  d  e  f  g  h");
+            
+        }
+
+        // Alterando cores
+        public static void imprimirPeca(Peca peca)
+        {
+
+            if (peca.cor == Cor.Branca)
+            {
+                // Imprimindo peça normalmente
+                Console.Write(peca);
+            } else
+            {
+                // Salvando cor da letra no terminal
+                ConsoleColor aux = Console.ForegroundColor;
+
+                // E alterando
+                Console.ForegroundColor = ConsoleColor.Magenta;
+
+                // Imprimindo peça
+                Console.Write(peca);
+
+                // Voltando pra cor original
+                Console.ForegroundColor = aux;
+            }
+
         }
     }
 }
